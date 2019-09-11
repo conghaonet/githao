3315dae756351540e2c15a2aabdf005c46d1eaea
conghaonet <hao.cong@app2m.com>
2019-09-12 00:11:00 +0800

    Navigator to profile page

--- lib/widgets/base_users.dart
+++ lib/widgets/base_users.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:githao/network/entity/user_entity.dart';
+import 'package:githao/pages/profile.dart';
+import 'package:githao/routes/profile_page_args.dart';
 import 'package:githao/utils/util.dart';
 import 'package:githao/widgets/load_more_data_footer.dart';
 import 'package:githao/widgets/loading_state.dart';
@@ -138,28 +140,43 @@ abstract class BaseUsersWidgetState<T extends BaseUsersWidget> extends State<T> 
   }
 
   Widget buildItem(int index) {
+    String _heroTag = _userEntities[index].login;
     return Container(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxHeight: 80,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: CachedNetworkImageProvider(_userEntities[index].avatarUrl),
-                    backgroundColor: Colors.black12,
-                  ),
-                  Text(
-                    _userEntities[index].login,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, ProfilePage.ROUTE_NAME, arguments: ProfilePageArgs(
+            userEntity: _userEntities[index],
+            heroTag: _heroTag,
+          ),);
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: 80,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Hero(
+                      //默认情况下，当在 iOS 上按后退按钮时，hero 动画会有效果，但它们在手势滑动时并没有。
+                      //要解决此问题，只需在两个 Hero 组件上将 transitionOnUserGestures 设置为 true 即可
+                      transitionOnUserGestures: true,
+                      tag: _heroTag,
+                      child: CircleAvatar(
+                        backgroundImage: CachedNetworkImageProvider(_userEntities[index].avatarUrl),
+                        backgroundColor: Colors.black12,
+                      ),
+                    ),
+                    Text(
+                      _userEntities[index].login,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),

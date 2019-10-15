126c4dbf3b30b7beadb2c4ffd55330d853efa98a
conghaonet <hao.cong@app2m.com>
2019-10-15 22:29:45 +0800

    refactor starred_repos.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: HomeDrawer(onClickDrawerMenu, _clickedDrawerMenu ?? _defaultMenu),
-      body: Container(
+      body: SafeArea(
         child: NestedScrollView(
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
             return <Widget>[
@@ -75,24 +75,11 @@ class _HomePageState extends State<HomePage> {
                     }
                   },
                 ),
-                titleSpacing: NavigationToolbar.kMiddleSpacing, //标题四周间距
-                primary: true,  //是否预留高度
+//                titleSpacing: NavigationToolbar.kMiddleSpacing, //标题四周间距
+//                primary: true,  //是否预留高度
                 snap:true,   //与floating结合使用
                 floating: true,//是否随着滑动隐藏标题，为true时，当有下滑手势的时候就会显示SliverAppBar
                 pinned: false,//为true时，SliverAppBar折叠后不消失
-/*
-                  expandedHeight: 0, // 展开的高度
-                  //性设置 SliverAppBar 的背景
-                  flexibleSpace: FlexibleSpaceBar(
-                    title: Text(S.current.myRepos),
-                    centerTitle: true,
-                    collapseMode: CollapseMode.parallax, // 背景折叠动画
-                    background: Image.asset(
-                      'images/banner1.jpg',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-*/
               ),
             ];
           },
--- lib/widgets/starred_repos.dart
+++ lib/widgets/starred_repos.dart
@@ -1,45 +1,93 @@
-
 import 'package:flutter/material.dart';
 import 'package:githao/network/api_service.dart';
 import 'package:githao/network/entity/repo_entity.dart';
 import 'package:githao/network/entity/user_entity.dart';
 import 'package:githao/resources/starred_filter_parameters.dart';
+import 'package:githao/widgets/base_list.dart';
+import 'package:githao/widgets/repo_item.dart';
 import 'package:githao/widgets/starred_repos_filter.dart';
 
-import 'base_repos.dart';
-
-class StarredReposWidget extends BaseReposWidget{
+class StarredReposWidget extends StatefulWidget{
   final UserEntity user;
-  StarredReposWidget({Key key, this.user, needLoadMore=true,}): super(key: key, needLoadMore: needLoadMore);
+  StarredReposWidget({Key key, this.user}): super(key: key);
   @override
   _StarredReposWidgetState createState() => _StarredReposWidgetState();
 }
 
-class _StarredReposWidgetState extends BaseReposWidgetState<StarredReposWidget> {
+class _StarredReposWidgetState extends State<StarredReposWidget> {
   int _groupSortIndex = 0;
-
-  @override
-  Future<List<RepoEntity>> getRepos(final int expectationPage){
-    String _sort = StarredFilterParameters.filterSortValueMap[_groupSortIndex][StarredFilterParameters.PARAMETER_NAME_SORT];
-    String _direction = StarredFilterParameters.filterSortValueMap[_groupSortIndex][StarredFilterParameters.PARAMETER_NAME_DIRECTION];
-    return ApiService.getUserStarredRepos(widget.user.login, page: expectationPage, sort: _sort, direction: _direction);
-  }
 
   void onClickFilterCallback(String group, int index) {
       if(group == StarredReposFilter.GROUP_SORT && _groupSortIndex != index) {
         setState(() {
           _groupSortIndex = index;
         });
-        refreshIndicatorKey.currentState.show();
       }
   }
 
-  @override
   Widget getFilter() {
     return StarredReposFilter(
-        this._groupSortIndex,
-        StarredFilterParameters.getFilterSortTextMap(context),
-        onClickFilterCallback);
+      this._groupSortIndex,
+      StarredFilterParameters.getFilterSortTextMap(context),
+      onClickFilterCallback,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _RepoList(widget.user, _groupSortIndex, key: ObjectKey(_groupSortIndex),),
+        Positioned(
+          bottom: 12,
+          right: 16,
+          child: FloatingActionButton(
+            child: Icon(Icons.sort),
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (BuildContext context) {
+                  return AnimatedPadding(
+                    padding: MediaQuery.of(context).viewInsets,  //边距（必要）
+                    duration: const Duration(milliseconds: 100), //时常 （必要）
+                    child: getFilter(),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+class _RepoList extends BaseListWidget {
+  final UserEntity user;
+  final int groupSortIndex;
+  _RepoList(this.user, this.groupSortIndex, {Key key}): super(key: key);
+  @override
+  _RepoListState createState() => _RepoListState();
+
+}
+
+class _RepoListState extends BaseListWidgetState<_RepoList, RepoEntity> {
+  @override
+  Widget buildItem(RepoEntity item, int index) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 4,),
+      child: RepoItem(item, tag: 'home_starred_repo',),
+    );
+  }
+
+  @override
+  Future<List<RepoEntity>> getDatum(int expectationPage) {
+    String _sort = StarredFilterParameters.filterSortValueMap[this.widget.groupSortIndex][StarredFilterParameters.PARAMETER_NAME_SORT];
+    String _direction = StarredFilterParameters.filterSortValueMap[this.widget.groupSortIndex][StarredFilterParameters.PARAMETER_NAME_DIRECTION];
+    return ApiService.getUserStarredRepos(widget.user.login, page: expectationPage, sort: _sort, direction: _direction);
   }
 
 }

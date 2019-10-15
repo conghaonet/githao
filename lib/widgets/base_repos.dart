a8228d8e25685856b9d967f5576f6017a4646b4f
conghaonet <hao.cong@app2m.com>
2019-10-15 19:29:54 +0800

    refactor base_repos.dart

--- lib/widgets/base_repos.dart
+++ lib/widgets/base_repos.dart
@@ -1,14 +1,9 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:githao/network/entity/repo_entity.dart';
-import 'package:githao/pages/profile.dart';
-import 'package:githao/pages/repo_home.dart';
-import 'package:githao/resources/custom_icons_icons.dart';
-import 'package:githao/resources/lang_colors.dart';
-import 'package:githao/routes/profile_page_args.dart';
 import 'package:githao/widgets/loading_state.dart';
 import 'package:githao/utils/util.dart';
+import 'package:githao/widgets/repo_item.dart';
 
 import 'load_more_data_footer.dart';
 import 'my_visibility.dart';
@@ -28,7 +23,6 @@ abstract class BaseReposWidgetState<T extends BaseReposWidget> extends State<T> 
   int _page = 1;
   StateFlag _loadingState = StateFlag.idle;
   bool _expectHasMoreData = true;
-  String _heroTimeTag;
   final GlobalKey<RefreshIndicatorState> refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
 
   Future<List<RepoEntity>> getRepos(final int expectationPage);
@@ -42,7 +36,6 @@ abstract class BaseReposWidgetState<T extends BaseReposWidget> extends State<T> 
   @override
   void initState() {
     super.initState();
-    _heroTimeTag = DateTime.now().microsecondsSinceEpoch.toString();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if(mounted) {
         refreshIndicatorKey.currentState.show();
@@ -178,164 +171,9 @@ abstract class BaseReposWidgetState<T extends BaseReposWidget> extends State<T> 
   }
 
   Widget _getRepoItem(int index) {
-    String avatarHeroTag = '${_heroTimeTag}_${_repos[index].owner.login}_$index';
-    return Card(
-      margin: EdgeInsets.only(left: 8, top: 8, right: 8, bottom: (index +1 == _repos.length) ? 8 : 0),
-      child: FlatButton(
-        padding: EdgeInsets.all(0),
-        onPressed: (){
-          Navigator.pushNamed(context, RepoHomePage.ROUTE_NAME, arguments: this._repos[index].fullName);
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              InkWell(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context, ProfilePage.ROUTE_NAME,
-                    arguments: ProfilePageArgs(
-                        userEntity: _repos[index].owner,
-                        heroTag: avatarHeroTag
-                    ),
-                  );
-                },
-                child: Hero(
-                  //默认情况下，当在 iOS 上按后退按钮时，hero 动画会有效果，但它们在手势滑动时并没有。
-                  //要解决此问题，只需在两个 Hero 组件上将 transitionOnUserGestures 设置为 true 即可
-                  transitionOnUserGestures: true,
-                  tag: avatarHeroTag,
-                  child: CircleAvatar(
-                    backgroundImage: CachedNetworkImageProvider(_repos[index].owner.avatarUrl),
-                    backgroundColor: Colors.black12,
-                  ),
-                ),
-              ),
-              SizedBox(width: 16,),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Offstage(
-                          offstage: !_repos[index].fork,
-                          child: Icon(CustomIcons.fork, color: Theme.of(context).primaryColor, size: 18,),
-                        ),
-                        Expanded(
-                          child: Text(_repos[index].name,
-                            maxLines: 2,
-                            style: TextStyle(fontSize: 16, color: Theme.of(context).primaryColor, fontWeight: FontWeight.w500),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 4,),
-                    MyVisibility(
-                      flag: _repos[index].description == null ? MyVisibilityFlag.gone : MyVisibilityFlag.visible,
-                      child: Text(_repos[index].description ?? '',
-                        maxLines: 4,
-                        softWrap: true,
-                        style: TextStyle(),
-                      ),
-                    ),
-                    SizedBox(height: 8,),
-                    Wrap(
-                      spacing: 16,
-                      runSpacing: 8,
-                      children: <Widget>[
-                        _getOwner(_repos[index].owner.login),
-                        _getIssues(_repos[index].openIssues),
-                        //抓包发现watchers和stargazersCount数值完全一样，应该是GitHub的bug
-//                      _getWatchers(_repos[index].watchers),
-                        _getStargazersCount(_repos[index].stargazersCount),
-                        _getForks(_repos[index].forks),
-                        _getLanguage(_repos[index].language),
-                        _getPushedTime(_repos[index].pushedAt),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-  Widget _getOwner(String owner) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.account_circle, color: Theme.of(context).primaryColor, size: 18,),
-        SizedBox(width: 2,),
-        Text(owner),
-      ],
-    );
-  }
-  Widget _getIssues(int issues) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.info, color: Theme.of(context).primaryColor, size: 18,),
-        SizedBox(width: 2,),
-        Text('$issues'),
-      ],
-    );
-  }
-  Widget _getStargazersCount(int count) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.stars, color: Theme.of(context).primaryColor, size: 18,),
-        SizedBox(width: 2,),
-        Text('$count'),
-      ],
-    );
-  }
-  Widget _getForks(int forks) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(CustomIcons.fork, color: Theme.of(context).primaryColor, size: 18,),
-        SizedBox(width: 2,),
-        Text('$forks'),
-      ],
-    );
-  }
-  Widget _getLanguage(String language) {
-    if(language != null && language.isNotEmpty) {
-      return Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            width: 14,
-            height: 14,
-            decoration: BoxDecoration(
-              color: langColors.getColor(language),
-              shape: BoxShape.circle,
-            ),
-          ),
-          SizedBox(width: 2,),
-          Text(language),
-        ],
-      );
-    } else {
-      return Offstage(offstage: true,);
-    }
-  }
-  Widget _getPushedTime(String time) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Icon(Icons.access_time, color: Theme.of(context).primaryColor, size: 18,),
-        SizedBox(width: 2,),
-        Text('Pushed on ${Util.getFriendlyDateTime(time) ?? ''}'),
-      ],
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 4,),
+      child: RepoItem(_repos[index], tag: 'base_repo_$index',),
     );
   }
   @override
